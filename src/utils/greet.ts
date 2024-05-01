a96e4554b947a618db21c295cbe4005246713e36
Vaaal <cassusval@gmail.com>
2024-05-01 10:46:24 +0200

    add swiper, init it and update somes values

--- src/utils/greet.ts
+++ src/utils/greet.ts
@@ -7,9 +7,9 @@ import { getPublishDate } from '@finsweet/ts-utils';
 export const greetUser = (name: string) => {
   const publishDate = getPublishDate();
 
-  console.log(`Hello ${name}!`);
+  console.log(`Bonjour ${name}!`);
   console.log(
-    `This site was last published on ${publishDate?.toLocaleDateString('en-US', {
+    `Ce site a été publié pour la dernière fois le ${publishDate?.toLocaleDateString('fr-FR', {
       year: 'numeric',
       month: 'long',
       day: '2-digit',
